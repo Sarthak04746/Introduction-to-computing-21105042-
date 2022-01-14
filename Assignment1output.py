179088d109cc208c1ef977b2aad467b91ba6e8f2
Sarthak04746 <97212856+Sarthak04746@users.noreply.github.com>
2022-01-14 15:28:44 +0530

    Add files via upload

---
+++ Assignment1output.py
@@ -0,0 +1,25 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:/Users/admin/Desktop/assignment 1.py ===============
+Enter first number -33
+Enter second number -89
+Enter third number - 67
+Average of three numbers is- 63.0
+enter Gross Income to the nearest penny -25700
+enter the number of Dependents -4
+Total tax is- 740.0
+Enter your SID-21105042
+Enter your name-Sarthak kalia
+Enter your Gender-(M,F,U) -  M
+Enter the course name-electronics and communication
+Enter the CGPA-9.85
+[21105042, 'Sarthak kalia', 'M', 'electronics and communication', 9.85]
+Enter the marks in Subject 1 = 98
+Enter the marks in Subject 2 = 91
+Enter the marks in Subject 3 = 87
+Enter the marks in Subject 4 = 100
+Enter the marks in Subject 5 = 100
+[87.0, 91.0, 98.0, 100.0, 100.0]
+['Magenta', 'Green', 'White', 'Pink', 'Yellow']
+['Magenta', 'Green', 'White', 'Purple', 'Yellow']
